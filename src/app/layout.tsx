c4ea4278ce304c16b70261afcc8c9cc2cf0e0510
jameson.gelarman <jameson.gelarman@cvsu.edu.ph>
2025-01-27 10:06:42 +0000

    merge issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import "./fonts/fonts.css"
+import "./fonts/fonts.css";
+import type { Metadata } from "next";
 import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Innotech",
   description: "Your web-solution buddy!",
+}
 
 export default function RootLayout({
     children,
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -134,7 +134,6 @@ export default {
   				}
   			}
   		}
-  	}
-  },
+  	},
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
