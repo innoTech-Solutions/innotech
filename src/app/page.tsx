cf9854a841b8fe9d5c489f82ef963c25decc3f43
Jardiv <jrlepardo3@gmail.com>
2025-01-29 19:54:38 +0800

    pricing almost done. Add more content/texts?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,39 @@
-import WhyChooseUs from '@/components/choose-us';
+import WhyChooseUs from "@/components/choose-us";
 import HeroSection from "@/components/HeroSection";
+import { Pricing } from "@/components/Pricing";
 
 export default function Home() {
-    return (
-        <div className="z-0 ">
-            {/* Hero section */}
-            <HeroSection/>
+	return (
+		<div className="z-0 ">
+			{/* Hero section */}
+			<HeroSection />
 
-            {/* Project Showcase */}
-            <div className="h-screen">
-                <h1>Project showcase</h1>
-            </div>
+			{/* Project Showcase */}
+			<div className="h-screen">
+				<h1>Project showcase</h1>
+			</div>
 
-            <WhyChooseUs/>
+			<WhyChooseUs />
 
-            {/* Testimonials */}
-            <div className="h-screen">
-                <h1>Testimonials</h1>
-            </div>
+			{/* Testimonials */}
+			<div className="h-screen">
+				<h1>Testimonials</h1>
+			</div>
 
-            {/* Meet the team */}
-            <div className="h-screen">
-                <h1>Meet the team</h1>
-            </div>
+			{/* Meet the team */}
+			<div className="h-screen">
+				<h1>Meet the team</h1>
+			</div>
 
-            {/* Pricing */}
-            <div className="h-screen">
-                <h1>PRICING</h1>
-            </div>
+			{/* Pricing */}
+			<div className="h-screen">
+				<Pricing />
+			</div>
 
-            {/* Footer */}
-            <div className="h-[25vh] bg-[#010116]">
-                <h1>FOOTER</h1>
-            </div>
-        </div>
-    );
+			{/* Footer */}
+			<div className="h-[25vh] bg-[#010116]">
+				<h1>FOOTER</h1>
+			</div>
+		</div>
+	);
 }
