934684d4f6e6bd2bde84be52a4cd61d28207ca6a
Jardiv <jrlepardo3@gmail.com>
2025-01-30 15:09:29 +0800

    fixed some data

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -9,18 +9,18 @@ const items = [
 	{
 		title: "Basic",
 		price: "PHP 5,000",
-		description: "For small businesses / personal use",
+		description: "A simple website, personal use",
 		features: ["Responsive", "1 - 3 pages", "Static Website"],
 	},
 	{
 		title: "Standard",
-		price: "PHP 10,000",
-		description: "For medium-sized businesses",
+		price: "PHP 15,000",
+		description: "For businesses",
 		features: ["Responsive", "5 - 10 pages", "Dynamic Website"],
 	},
 	{
 		title: "Custom",
-		price: "PHP 12,000",
+		price: "PHP 20,000",
 		description: "For large businesses",
 		features: ["Responsive", "10 - 15 pages", "Dynamic Website"],
 	},
