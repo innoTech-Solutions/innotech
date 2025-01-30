9d9cf47961389a339725296c5f37d246f0e58ee0
jameson.gelarman <jameson.gelarman@cvsu.edu.ph>
2025-01-30 14:41:32 +0000

    merge pricing complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,23 @@ export default function Home() {
     return (
         <div className="z-0 ">
             <HeroSection/>
+
             {/* Project Showcase */}
             <div className="h-screen">
                 <h1>Project showcase</h1>
             </div>
             <WhyChooseUs/>
+            
             {/* Testimonials */}
             <div className="h-screen">
                 <h1>Testimonials</h1>
             </div>
+
             {/* Meet the team */}
             <div className="h-screen">
                 <h1>Meet the team</h1>
             </div>
-            {/* Pricing */}
+
             <div className="h-screen">
 				<Pricing />
             </div>
